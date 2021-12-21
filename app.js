e17cc0f3d13b4f42edab980b39ee7a1913a1db74
Otabek <otabekabdulazizov282@gmail.com>
2021-12-21 16:31:08 +0500

    css

--- app.js
+++ app.js
@@ -1,91 +1,228 @@
- // This first lesson
+"use sctrict"
 
- // new Vue({
- //     el: '#app',
- //     data: {
- //         name: 'hello everone',
- //         surname: 'Read again',
- //         number: 20 ** 2 + 2 ** 2,
- //         olver: false,
- //         string: 'This is info more question about '
- //     },
- //     methods: {
- //         changeName: function (event) {
- //             this.name = event.target.value;
+// This first lesson
 
- //         },
- //         colors: function (lok) {
- //             this.surname = lok.target.value
- //         },
- //         sayAnything: function () {
- //             return "I am otabek"
- //         }
- //     }
- //  });
+// new Vue({
+//     el: '#app',
+//     data: {
+//         name: 'hello everone',h
+//         surname: 'Read again',
+//         number: 20 ** 2 + 2 ** 2,
+//         olver: false,
+//         string: 'This is info more question about '
+//     },
+//     methods: {
+//         changeName: function (event) {
+//             this.name = event.target.value;
 
-
- // This second lesson
-
- //  new Vue({
- //      el: '#app',
- //      data: {
- //          url: "http://google.com",
- //          Telegram: 'https://t.me/@king_07777'
- //      }
- //  });
-
- // This third lesson
+//         },
+//         colors: function (lok) {
+//             this.surname = lok.target.value
+//         },
+//         sayAnything: function () {
+//             return "I am otabek"
+//         }
+//     }
+//  });
 
 
- //  new Vue({
- //      el: '#app',
- //      data: {
- //          link: '<a href="https://t.me/@king_07777">Click</a>'
- //      }
- //  });
+// This second lesson
 
- // This fourth lesson
+//  new Vue({
+//      el: '#app',
+//      data: {
+//          url: "http://google.com",
+//          Telegram: 'https://t.me/@king_07777',
+//          call: "hey guys"
+//      }
+//  });
 
 
- //  new Vue({
- //      el: '#app',
- //      data: {
- //          counter: 0,
- //          counter2: 0
- //      },
- //      methods: {
- //          RiseCounter: function () {
- //              this.counter--
- //          },
- //          hover: function (event) {
- //              event.target.style.color = 'red'
- //          }
- //      }
- //  });
+// This third lesson
 
- // This fifth lesson
 
- new Vue({
-     el: '#app',
-     data: {
-         adder: 0,
-         Divison: "multply"
-     },
-     methods: {
-         RiseNumber: function (num, event) {
-             this.adder += num
-             if (num === 5) {
-                 event.target.style.color = "red"
-             } else if (num == 10) {
-                 event.target.style.color = "blue"
-             }
+//  new Vue({
+//      el: '#app',
+//      data: {
+//          link: '<a href="https://t.me/@king_07777">Click</a>'
+//      }
+//  });
 
-         },
-         RiseNumber2: function (ad, event) {
-             this.adder += ad
-             if (ad == 10) {
-                 event.target.style.color = "greeng"
-             }
-         }
-     }
- })
+// This fourth lesson
+
+
+//  new Vue({
+//      el: '#app',
+//      data: {
+//          counter: 0,
+//          counter2: 0
+//      },
+//      methods: {
+//          RiseCounter: function () {
+//              this.counter--
+//          },
+//          hover: function (event) {
+//              event.target.style.color = 'red'
+//          }
+//      }
+//  });
+
+// This fifth lesson
+
+//   new Vue({
+//       el: '#app',
+//       data: {
+//           adder: 0,
+//           Divison: "multply"
+//       },
+//       methods: {
+//           RiseNumber: function (num, event) {
+//               this.adder += num
+//               if (num === 5) {
+//                   event.target.style.color = "red"
+//               } else if (num == 10) {
+//                   event.target.style.color = "blue"
+//               }
+
+//           },
+//           RiseNumber2: function (ad, event) {
+//               this.adder += ad
+//               if (ad == 10) {
+//                   event.target.style.color = "greeng"
+//               }
+//           }
+//       }
+//   });
+
+// This sixth lesson
+
+// new Vue({
+//     el: '#app',
+//     data: {
+//         X: 0,
+//         Y: 0
+//     },
+//     methods: {
+//         moverhandle: function (event) {
+//             this.X = event.clientX
+//             this.Y = event.clientY
+//         }
+//     }
+// });
+
+// This sevevnth lesson
+
+// new Vue({
+//     el: '#app',
+//     data: {
+
+//     },
+//     methods: {
+//         alertcall: function (event) {
+
+//             if (event.keyCode === 13) {
+//                 alert(event.target.value)
+//             }
+//         }
+//     }
+// });
+
+// This eighth lesson
+
+// new Vue({
+//     el: '#app',
+//     data: {
+//         sanash: 0,
+//         url:'https:/google.com'
+//     },
+//     methods: {
+
+//     }
+// });
+
+// This ninth lesson
+
+// new Vue({
+//     el: "#app",
+//     data: {
+//         inputform: "hello"
+//     }
+// });
+
+
+// This tenth lesson
+
+// new Vue({
+//     el: "#app",
+//     data: {
+//         isActive: false,
+//         color: ''
+//     },
+//     computed: {
+//         getCssClasses: function () {
+//             return {
+//                 'red': this.isActive,
+//                 'green': !this.isActive
+//             }
+//         }
+//     },
+
+// Bunda sekin ishlaydi tepadgi bundan yaxshi ishlaydi
+//     methods: {
+//         getCssClasses: function () {
+//             return {
+//                 'red': this.isActive,
+//                 'green': !this.isActive
+//             }
+//         }
+//     }
+// });
+
+// This eleventh lesson
+
+// new Vue({
+//     el: "#app",
+//     data: {
+//         color: "green",
+//         height: 100,
+//         width: 100,
+
+//     },
+//     computed: {
+//         circleClass: function () {
+//             return {
+//                 'background': this.color,
+//                 'height': this.height + 'px',
+//                 'width': this.width + 'px'
+//             }
+//         }
+//     }
+// });
+
+// This twelveth lesson
+
+// new Vue({
+//     el: '#app',
+//     data: {
+
+//         Visible: false
+//     }
+// });
+
+// This thirteenth lesson
+
+// new Vue({
+//     el: "#app",
+//     data: {
+//        isVisibel: true
+//     }
+// });
+
+// This fourteenth lesson
+
+new Vue({
+    el: '#app',
+    data: {
+
+    }
+})
